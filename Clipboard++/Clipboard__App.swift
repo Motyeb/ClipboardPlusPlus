500afa16fc1139aa458d03df7e5172aac50bc150
Thomas Sheppard <tsheppard346@gmail.com>
2026-04-10 20:05:46 +0100

    Initial Commit

---
+++ Clipboard++/Clipboard__App.swift
@@ -0,0 +1,17 @@
+//
+//  Clipboard__App.swift
+//  Clipboard++
+//
+//  Created by Thomas Sheppard on 10/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Clipboard__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
